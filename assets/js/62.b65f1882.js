51af131a4390f41fbd919fb29c921cb92672d3b8
root <root@cloud-s4w3ad-1ttn.novalocal>
2024-01-23 11:23:13 +0800

    change back to reco v1.x

---
+++ assets/js/62.b65f1882.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[62],{471:function(t,s,e){"use strict";e.r(s);var n=e(2),l=Object(n.a)({},(function(){var t=this._self._c;return t("ContentSlotsDistributor",{attrs:{"slot-key":this.$parent.slotKey}},[t("p",[this._v("编译内核需要的库总结如下:\nlibncurses-dev flex bison libssl-dev libelf-dev")])])}),[],!1,null,null,null);s.default=l.exports}}]);
